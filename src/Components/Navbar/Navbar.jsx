943a33cbc8a0691b3d55fb581d0e4e82ad2ea1de
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-04-16 08:40:37 +0600

    Log in and Register Page added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,33 +10,27 @@ const Navbar = () => {
         <li><Link>Contact Us</Link></li>
     </>
     return (
-        <div> <div>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {navItems}
-                        </ul>
+
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <a className="font-bold text-xl"><span className="text-3xl text-blue-500">H</span>ome <span className="text-3xl text-blue-500">H</span>unter</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 font-bold text-xl">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {navItems}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn font-bold text-xl">Log In</a>
-                </div>
+                <a className="font-bold text-xl"><span className="text-3xl text-blue-500">H</span>ome <span className="text-3xl text-blue-500">H</span>unter</a>
             </div>
-        </div>
-            
-            {/* ----------------- */}
-           
-
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 font-bold text-xl">
+                    {navItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link to={'/login'}> <a className="btn font-bold text-xl">Log In</a> </Link>
+            </div>
         </div>
     );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,8 @@ import Root from "../Components/Root/Root";
 import Home from "../Components/Home/Home";
 import AllProperty from "../Components/AllProperty/AllProperty";
 import PropertyDetails from "../Components/PropertyDetails/PropertyDetails";
+import LogIn from "../Components/LogIn/LogIn";
+import Register from "../Components/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -22,6 +24,14 @@ const router = createBrowserRouter([
             path:'/property/:id',
             loader: ()=>fetch('data.json'),
             element:<PropertyDetails></PropertyDetails>
+        },
+        {
+            path:'/login',
+            element:<LogIn></LogIn>
+        },
+        {
+            path:'/register',
+            element:<Register></Register>
         },
       ]
       
